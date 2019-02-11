df33c20919807145d4a52e747aa57b7a5bcda556
ohjho <jho.yvr+git@gmail.com>
2019-02-11 11:04:48 +0800

    removed old data file, added placeholder for data import script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A short study on the return of stock prices vs news/ tweets sentiments
 
 ## Packages Used/ Credit
-We stands on the shoulders of Giants! :muscle:
+We stand on the shoulders of Giants! :muscle:
 
 To install the various packages we used:
 ```
@@ -11,6 +11,7 @@ $ python -m spacy download en_core_web_sm
 ```
 
 * News Article Summarization using **TextRank** algorithim: [summa](https://github.com/summanlp/textrank), [sumy](https://github.com/miso-belica/sumy)
+  * [review of Extractive Text Summarization Techniques](https://medium.com/@ondenyi.eric/extractive-text-summarization-techniques-with-sumy-3d3b127a0a32)
 * Relevancy Score using the help of [spaCy](https://spacy.io/usage/linguistic-features#section-named-entities)'s **Named Entities** function
 * Sentiment Score generated using [vaderSentiment](https://github.com/cjhutto/vaderSentiment) with [stock_market_lexicon](https://github.com/nunomroliveira/stock_market_lexicon); for installation, see [below]()
 
--- sentiment_utils.py
+++ sentiment_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 # 1. Data Cleaning
 import re
 
@@ -56,10 +57,26 @@ from nltk import sent_tokenize
 
 
 
+def FilterOutCssJs(x):
+    x = re.sub(r'Read the original article.+','', x)
+    x = re.sub(r'Shutterstock.+photo\@media.+\(Reuters\) \-','',x)
+    x = re.sub(r'<\^.+', ' ', x)
+    x = re.sub(r'max.+important;}}','',x)
+    x = re.sub(r'=======================================.+','',x)
+    x = re.sub(r'\(?function\(\).+', '', x)
+    x = re.sub(r'\(Reporting.+', '', x)
+    x = re.sub(r'makeAd.+\;', '', x)
+    x = re.sub(r'Source: Shutterstock', '',x)
+    x = re.sub(r'Reuters@media.+ \-', '', x)
+
+    return x
+
 def GetCleanText(inText):
     outText = re.sub(r"(\n)|(\r)|('\')", '', inText)
     outText = outText.replace(u'\xa0',' ').encode('utf-8')
     outText = outText.decode('utf-8')
+
+    outText = FilterOutCssJs( outText )
 
     return outText
 
@@ -72,7 +89,10 @@ def GetSummary(inText, method = 'summa'):
         LsaSum = LsaSummarizer( Stemmer(lang))
         LsaSum.stop_words = get_stop_words(lang)
 
-        summary_sumy = LsaSum(parser.document, sent_count)
+        sumy_sents = LsaSum(parser.document, sent_count)
+        summary_sumy = ' '.join(
+                        [str(sent) for sent in sumy_sents]
+                        )
         return summary_sumy
 
     elif method == 'summa':
@@ -85,17 +105,23 @@ def GetSummary(inText, method = 'summa'):
 def GetRelavency(inText, kwList, debugmode = False):
     nlp = spacy.load('en_core_web_sm')
     doc = nlp(inText)
+    sent_text = sent_tokenize( inText )
 
-    rSource = 0
+    rScore = 0
     for ent in doc.ents:
         ent_text = ent.text.lower().strip()
         if ent_text in [kw.lower() for kw in kwList]:
-            rSource += 1
+            rScore += 1
 
             if debugmode:
                 print(f'Found {ent_text} relavent.')
 
-    return rSource
+    freq = 0
+    if len(sent_text)>0:
+        freq = rScore / len(sent_text)
+
+    #return {'count': rScore, 'sent_count': len(sent_text), 'freq': freq}
+    return rScore
 
 def GetSentimentScore(inText, method = 'bespoke'):
     if method == 'bespoke':
@@ -105,11 +131,13 @@ def GetSentimentScore(inText, method = 'bespoke'):
 
     sent_text = sent_tokenize( inText)
 
-    sent_count = 0
-    sent_sum = 0
-    for sent in sent_text:
-        sent_sum += SIA.polarity_scores(sent)['compound']
-        sent_count += 1
-
-    SIA_Score = sent_sum/ sent_count
+    l_score = [ SIA.polarity_scores(sent)['compound'] for sent in sent_text]
+    SIA_Score = np.mean(l_score)
+    # sent_count = 0
+    # sent_sum = 0
+    # for sent in sent_text:
+    #     sent_sum += SIA.polarity_scores(sent)['compound']
+    #     sent_count += 1
+    #
+    # SIA_Score = sent_sum/ sent_count
     return SIA_Score
